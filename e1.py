a1d9624e58aa84c085e3b3e966a2dcd51f17566b
Leopoldo Santos <santosg572@gmail.com>
2017-09-11 06:35:44 -0500

    respado

---
+++ e1.py
@@ -0,0 +1,9 @@
+fil = open('t1.txt', 'r')
+dd = fil.read()
+fil.close()
+print dd
+print type(dd)
+print len(dd)
+
+
+
---
+++ e3.py
@@ -0,0 +1,16 @@
+fil = open('t1.txt', 'r')
+dd = fil.readline()
+print dd
+
+dd = fil.readline()
+print dd
+
+fil.close()
+
+print dd
+print type(dd)
+print len(dd)
+
+
+
+
